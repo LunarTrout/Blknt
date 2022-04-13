8fa8b57106ee0146f669f64ea670d45e0c236d16
BoarOFF <epiccoll783@gmail.com>
2022-04-13 22:46:32 +0300

    it's all 0_0

--- app/src/androidTest/java/com/example/blknt/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/blknt/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.blcnt", appContext.packageName)
+        assertEquals("com.example.blknt", appContext.packageName)
     }
 }
